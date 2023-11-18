8b80c8bd68220557d41cbf7fe1e2d48d68bf2f3e
arunkapila <arunj.kapila@gmail.com>
2023-11-18 11:59:04 +0000

    Fixed resource pack not working

--- src/main/java/com/starshootercity/zombies/ResourcePackSender.java
+++ src/main/java/com/starshootercity/zombies/ResourcePackSender.java
@@ -1,6 +1,7 @@
 package com.starshootercity.zombies;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,10 +12,8 @@ public class ResourcePackSender implements Listener {
         String url = InfectionIsland.getInstance().getConfig().getString("resource-pack.url");
         boolean forceAccept = InfectionIsland.getInstance().getConfig().getBoolean("resource-pack.enforce");
         String packMessage = InfectionIsland.getInstance().getConfig().getString("resource-pack.message");
-        String sha1 = InfectionIsland.getInstance().getConfig().getString("resource-pack.sha1");
+        Bukkit.broadcast(Component.text(1));
         if (url == null || url.equals("") || packMessage == null) return;
-        if (sha1 == null) {
-            event.getPlayer().setResourcePack(url, null, Component.text(packMessage), forceAccept);
-        } else event.getPlayer().setResourcePack(url, sha1, forceAccept, Component.text(packMessage));
+        event.getPlayer().setResourcePack(url, null, Component.text(packMessage), forceAccept);
     }
 }
