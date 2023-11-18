6e605cd298e8cfb284917cf734980ce0b9ead1d9
arunkapila <arunj.kapila@gmail.com>
2023-11-18 16:52:54 +0000

    Removed debug message

--- src/main/java/com/starshootercity/zombies/ResourcePackSender.java
+++ src/main/java/com/starshootercity/zombies/ResourcePackSender.java
@@ -1,7 +1,6 @@
 package com.starshootercity.zombies;
 
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,7 +11,6 @@ public class ResourcePackSender implements Listener {
         String url = InfectionIsland.getInstance().getConfig().getString("resource-pack.url");
         boolean forceAccept = InfectionIsland.getInstance().getConfig().getBoolean("resource-pack.enforce");
         String packMessage = InfectionIsland.getInstance().getConfig().getString("resource-pack.message");
-        Bukkit.broadcast(Component.text(1));
         if (url == null || url.equals("") || packMessage == null) return;
         event.getPlayer().setResourcePack(url, null, Component.text(packMessage), forceAccept);
     }
